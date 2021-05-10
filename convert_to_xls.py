f4faf1fb31216a3952c6d9d75cf4d02da9627c77
stringbottle <stringbottle.mail@gmail.com>
2021-05-10 23:03:35 +0900

    add collect temp info and complete main function

--- convert_to_xls.py
+++ convert_to_xls.py
@@ -1,12 +1,15 @@
-
+import io
 import pickle
 
+import numpy as np
 import os.path as osp 
 import pandas as pd
 
 from glob import glob 
 from pathlib import Path
 from tqdm import tqdm
+from module.disp_measure import homography_transformation
+from module.utils import str2array
 
 
 data_root = '/media/sss/Seagate Backup Plus Drive/NIA_Smart_Monitoring/folderized_data'
@@ -29,7 +32,7 @@ img_sns_list = ['002', '005', '008', '011', '014', '017', '023', '026',
 
 # search imgs in subdirectories 
 
-def search_directories_by_sensor(data_root, sns_type, sns_list) : 
+def search_directories_by_sensor(data_root, sns_type, sns_list=None) : 
     
     """
     Args : 
@@ -43,10 +46,20 @@ def search_directories_by_sensor(data_root, sns_type, sns_list) :
     """
     directories_by_sensor = [] 
 
-    for sns_num in tqdm(sns_list, desc='Search Directories by Sensor : '): 
-        directories = glob(osp.join(data_root, sns_type, sns_num, '*', '*', '*'))
-        directories_by_sensor.append(directories)
+    if sns_list : 
+        for sns_num in tqdm(sns_list, desc='Search Directories by Sensor : '): 
+            directories = glob(osp.join(data_root, sns_type, sns_num, '*', '*', '*'))
+            directories_by_sensor.append(directories)
+        
+    else : 
+        sns_list = glob(osp.join(data_root, sns_type, '*'))
+        for sns_num_ in tqdm(sns_list, desc='Search Directories by Sensor : '):
+            sns_num = osp.basename(sns_num_)
+            directories = glob(osp.join(data_root, sns_type, sns_num, '*', '*', '*'))
+            directories_by_sensor.append(directories)
+
     return directories_by_sensor
+    
 
 
 def filter_directories_by_date(directories_by_sensor, filter_year, filter_month, filter_date):
@@ -81,6 +94,7 @@ def filter_directories_by_date(directories_by_sensor, filter_year, filter_month,
 
     return filtered_list
 
+
 def create_sns_info_df(sns_dict):
 
     """
@@ -108,18 +122,68 @@ def create_sns_info_df(sns_dict):
 
     return sns_info_df
 
-def get_latest_log_folder(log_folder):
+
+def get_src_circles(sns_num):
+
+    """
+    Args : 
+        sns_num (int or float) : sensor number 
+
+    Returns : 
+        src_circles (np.array) : center coordinates of source circles 
+        
+    """
+    # default source image number 
+    year = '2020'
+    month = '10'
+    date = '14'
+    time = '16'
+
+    # exceptions     
+    if sns_num == '011':
+        time = '01'
+    elif sns_num == '041':
+        time = '16'
+    elif sns_num == '135':
+        time = '07'
+    elif sns_num == '071':
+        time = '11'
+    elif sns_num == '155':
+        time = '23'
+    
+    src_circles_folder = osp.join(data_root, sns_type, sns_num, year, month, date) 
+
+    latest_log = get_latest_log_folder(src_circles_folder)
+
+    circles_pickle_filename = osp.join(latest_log, 'circle_detection_results.pickle')
+
+    with open(circles_pickle_filename , 'rb') as readfile:
+        src_circles_dict = pickle.load(readfile)
+
+    img_key = 'Img_{}_{}{}{}_{}0100.jpg'.format(sns_num, year, month, date, time)
+
+    return src_circles_dict[img_key]
+
+
+def get_latest_log_folder(date_folder):
 
     """
     Args :
-        log_folder (str) : directory of log folder 
+        date_folder (str) : directory of date_folder 
 
     Return : 
         latest_log (str) : latest log folder name 
 
     """
 
-    log_folders_by_time = glob(osp.join(log_folder, '*'))
+    log_folder = osp.join(date_folder, 'logs')
+    exist_log_folder = Path(log_folder).exists()
+
+    if exist_log_folder : 
+        log_folders_by_time = glob(osp.join(log_folder, '*'))
+    else : 
+        print("There is no log folder exists under {}.".format(date_folder))
+        return None
 
     if len(log_folders_by_time) > 1 : 
         log_folders_by_time.sort
@@ -129,6 +193,16 @@ def get_latest_log_folder(log_folder):
 
     return latest_log
 
+
+def get_latest_circle_pkl(log_folders_by_time):
+    
+    for latest_log in reversed(log_folders_by_time):
+        circles_pickle_filename = osp.join(latest_log, 'circle_detection_results.pickle')
+        if Path(circles_pickle_filename).exists():
+            return circles_pickle_filename
+    return None
+
+
 def collect_disp_info(img_folder_list):
 
     """
@@ -136,66 +210,126 @@ def collect_disp_info(img_folder_list):
         img_folder_list (list) : directory list of image folders to scan 
 
     Return: 
-        disp_info_list (list) : collected displacement list 
+        disp_info_dict (dict) : collected displacement dict 
     """
 
-    disp_info_list = []
+    disp_info_dict = {}
 
-    for img_folder in tqdm(img_folder_list): 
+    for img_folder in tqdm(img_folder_list, desc="Collecting displacement data : "): 
         sns_num = img_folder[0].split('/')[-4]
         disp_info = pd.DataFrame(columns=['datetime', 'circles','x_disp','y_disp'])
 
         for date_folder in img_folder : 
-
+            
+            # latest_log = get_latest_log_folder(date_folder)
             log_folder = osp.join(date_folder, 'logs')
             exist_log_folder = Path(log_folder).exists()
 
             if exist_log_folder : 
-                latest_log = get_latest_log_folder(log_folder)
+                log_folders_by_time = glob(osp.join(log_folder, '*'))
+            else : 
+                print("There is no log folder exists under {}.".format(date_folder))
+                continue
 
-            circles_pickle_filename = osp.join(latest_log, 'circle_detection_results.pickle')
-            circle_detection_folder = osp.join(latest_log, 'circle_detection_result_images')
-            # displacement_dict_filename = osp.join(latest_log, 'displacement_measurement.json')
-            circle_detection_list = glob(osp.join(circle_detection_folder, "*.jpg"))
+            log_folders_by_time.sort
+
+            for latest_log in reversed(log_folders_by_time):
+                circles_pickle_filename = osp.join(latest_log, 'circle_detection_results.pickle')
+                if Path(circles_pickle_filename).exists():
+                    break
+
+            if not Path(circles_pickle_filename).exists():
+                print("There is no circle_detection_results exists under {}.".format(latest_log))
+                continue
+
 
             with open(circles_pickle_filename , 'rb') as readfile:
                 circles_dict = pickle.load(readfile)
             
             for img_name, circles in circles_dict.items() : 
-                
 
-            for circle_detection_image in circle_detection_list:
-                image_basename = osp.basename(circle_detection_image)
-                image_basename_no_ext = image_basename.split('.')[0]
-                sns_type, _ , year_month_date, hh_mm_ss = image_basename_no_ext.split('_')
-                dest_circles = circles_dict[image_basename]
-                x_disp = displacement_dict[image_basename]['x_disp']
-                y_disp = displacement_dict[image_basename]['y_disp']
+                _, _ , year_month_date, hh_mm_ss = img_name.split('_')
+                
+                if not isinstance(circles, str) : 
+                    src_circles = np.asarray(get_src_circles(sns_num))
+                    circles = np.asarray(circles)
+                    disp_result = homography_transformation(src_circles[:, :2], circles[:, :2])
+                    x_disp, y_disp = float(disp_result[0]), float(disp_result[1])
+
+                else : 
+                    x_disp, y_disp = None, None 
 
                 disp_info = disp_info.append({'datetime' : str(year_month_date) + str(hh_mm_ss[:2]),
-                                              'circles' : dest_circles,
-                                              'x_disp': x_disp,
-                                              'y_disp': y_disp }, ignore_index = True)
-    disp_info_list.append(disp_info)    
+                                            'circles' : circles,
+                                            'x_disp': x_disp,
+                                            'y_disp': y_disp }, ignore_index = True)
+                    
+        disp_info_dict[sns_num] = disp_info
 
-    return None
+    return disp_info_dict
 
+
+def collect_temp_info(tmp_folder_list): 
+
+    tmp_dict = {}
+    for tmp_folder in tqdm(tmp_folder_list, desc='Collecting Temperature Data : '): 
+        
+        sns_num = tmp_folder[0].split('/')[-4]
+        tmp_info = pd.DataFrame(columns=['datetime', 'temp'])
+        
+        for date_folder in tmp_folder: 
+            
+            tmp_list = glob(osp.join(date_folder, '*.tpr'))
+            for tmp_file in tmp_list : 
+                tmp_measured = None
+                f = io.open(tmp_file, mode="r", encoding="utf-8")
+                txt = f.read()
+                if txt :
+                    tmp_measured = str2array(txt)[1]
+
+                tmp_basename = osp.basename(tmp_file)
+                # sns_type = tmp_basename[:3]
+                # sns_id = tmp_basename[4:7]
+                date = tmp_basename[8:16]
+                time_ = tmp_basename[17:23]
+                
+                tmp_info = tmp_info.append({'datetime' : str(date) + str(time_[:2]) , 
+                                            'temp' : tmp_measured} , ignore_index = True)
+                
+        tmp_dict[sns_num] = tmp_info
+
+    return tmp_dict
 
 
 def main(): 
 
     img_sns_dirs = search_directories_by_sensor(data_root, sns_type, img_sns_list )
 
+    tmp_sns_dirs = search_directories_by_sensor(data_root, 'Tmp')
+
     filtered_img_sns_dirs = filter_directories_by_date(img_sns_dirs, 2020, 9, 1)
+    filtered_tmp_sns_dirs = filter_directories_by_date(tmp_sns_dirs, 2020, 9, 1)
 
     # create excel format dataframe from sns information
     with open('data_tmp.pkl', "rb") as f:
         sns_dict = pickle.load(f)
 
     sns_info_df = create_sns_info_df(sns_dict)
+    
+    tmp_info_dict = collect_temp_info(filtered_tmp_sns_dirs)
+    disp_info_dict = collect_disp_info(filtered_img_sns_dirs)
+    
+    writer = pd.ExcelWriter('2021.05.10.xlsx', engine='xlsxwriter')
 
-    collect_disp_info(filtered_img_sns_dirs)
+    sns_info_df.to_excel(writer, sheet_name= 'snsr_info')
 
+    for sns_num, disp_data in disp_info_dict.items():
+        disp_data.to_excel(writer, sheet_name= sns_num)
+
+    for sns_num, tmp_data in tmp_info_dict.items():
+        tmp_data.to_excel(writer, sheet_name= sns_num)
+
+    writer.save()
 
 
 if __name__ == "__main__" : 
